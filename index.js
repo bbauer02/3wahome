8812322534ad2af920b5597c8542a40d194fedff
unknown <wumi.olabisi@gmail.com>
2022-10-28 10:31:46 +0200

    Root pour Appart et accueil séparé

--- index.js
+++ index.js
@@ -5,45 +5,17 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(4000);
 
-
-/*
 app.get("/", function (request, response) {
 
-    response.render("homePage", {
-        data: data
-    });
-});*/
-
-app.get("/appartements", function (request, response) {
-
-    response.render("testPage");
+    response.render("HomePage");
 });
-app.get("/", (request, response) => {
+
+app.get("/appartements", (request, response) => {
     fetchData(object => {
-        response.render("HomePage", { object: object });
+        response.render("AppartPage", { object: object });
     })
 
 });
-function getAppart() {
-    const http = require('http');
-
-    http.get('http://localhost:3000/bien', (resp) => {
-        let data = '';
-
-        // Un morceau de réponse est reçu
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // La réponse complète à été reçue. On affiche le résultat.
-        resp.on('end', () => {
-            console.log(JSON.parse(data).explanation);
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
-}
 
 function fetchData(callback) {
     fetch("http://localhost:3000/bien") //Could change this to a find request to the data base directly like Teacher.find()...
